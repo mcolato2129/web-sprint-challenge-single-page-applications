5cd8c88746ca478beaa1103c0f19aeb803d96465
Marlon Colato <mcolato2104@gmail.com>
2023-07-02 14:30:11 -0400

    inported Yup, useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { useState,useEffect } from "react";
 import { Routes, Route, Link } from 'react-router-dom';
+import * as yup from 'yup';
 
 
-const style = { margin: '1rem', padding: '0.5rem', border: '2px solid black' }
+const schema = yup.object().shape({
+  id: yup.string().required('name must be at least 2 characters').min(2, 'name must be at least 2 characters' )
+});
+
+const  style = {margin: ' rem', padding: '0.5rem', border: '2 px solid black' }
 
 const Home = (props) => {
   return (
@@ -13,6 +19,8 @@ const Home = (props) => {
 }
 
 const PizzaForm = (props) => {
+  const [form, setFrom] = useState({id: ''})
+
   const handleSubmit = (event) => {
     event.preventDefault();
 
@@ -20,21 +28,28 @@ const PizzaForm = (props) => {
     // const size = event.target.elements.size-dropdown.value;
 
   }
+
+  useEffect(() => {
+    schema.isValid()
+  },[])
+
   return (
     <div style={{ ...style, borderColor: 'red' }}>
       <h1>Pizza Form Thing...I guess. Yeah. Ok. Good.</h1>
       <form id='pizza-form' onSubmit={handleSubmit}>
         <input 
+          type= 'text'
           placeholder='Name'
+          value={form.id}
           id='name-input'
           minLength='2'
         />
         <br/>
         <label>Pizza Size</label>
         <select id='size-dropdown'>
-          <option>small</option>
-          <option>medium</option>
-          <option>large</option>
+          <option value='1'>small</option>
+          <option value='2'>medium</option>
+          <option value='3'>large</option>
         </select>
         <br/>
         <input type='checkbox'/>
